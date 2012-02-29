c6f2272a717a3238e78b49b0ef00c9afcdfb22e0
Andrew Elliott <contact@awegroup.com>
2012-02-28 23:06:24 -0800

    Precomplie Blueprint in production

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 ThirdApp::Application.initialize!
+
+# Precompile Blueprint CSS
+  config.assets.precompile += [ "blueprint/*.css" ]
+end
